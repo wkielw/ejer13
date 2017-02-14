410fd0341176b4a7d35965f93f823f932b14c213
Kimera <Kimera@192.168.1.33>
2017-02-15 00:22:36 +0100

    Segundo commit

--- ejer13/src/Vehiculos/Vehiculo.java
+++ ejer13/src/Vehiculos/Vehiculo.java
@@ -6,6 +6,11 @@ public class Vehiculo {
 	private int portas;
 	private int velocidad_actual=0;
 	private int velocidad_maxima;
+	private int marxes=5;
+	private int marxe_actual=0;
+	
+	
+	
 	
 	public static int acelerar(int velocidad){
 		if(velocidad<120){
@@ -19,7 +24,16 @@ public class Vehiculo {
 		}
 		return velocidad;
 	} 
-	
+	public static void puxarMarxe(){
+		
+		
+		
+	}
+	public static void baixarMarxe(){
+		
+		
+		
+	}
 	
 	
 	
